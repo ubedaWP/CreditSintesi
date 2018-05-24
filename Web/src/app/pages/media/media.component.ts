6cf69b111be72582d1f366f396631304a8d9a27d
Unknown <guillemvl@hotmail.com>
2018-05-24 18:19:26 +0200

    Canvis Home definitius de veritat

--- Web/src/app/pages/media/media.component.ts
+++ Web/src/app/pages/media/media.component.ts
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import { Component, OnInit, Input} from '@angular/core';
-=======
 import { Component, OnInit } from '@angular/core';
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml, SafeStyle, SafeScript, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
->>>>>>> master
 
 @Component({
   selector: 'app-media',
@@ -13,29 +9,10 @@ import { DomSanitizer, SafeHtml, SafeStyle, SafeScript, SafeUrl, SafeResourceUrl
 })
 export class MediaComponent implements OnInit {
 
-<<<<<<< HEAD
-  images;
-
-  constructor() {
-    this.images = [
-    	{"url":"https://i2-prod.mirror.co.uk/incoming/article11509896.ece/ALTERNATES/s1200/world-of-warcraft-battle-for-azeroth.png"},
-    	{"url":"https://cdn.wccftech.com/wp-content/uploads/2018/01/World-of-Warcraft-Battle-for-Azeroth-preorder-free-110-character-740x416.jpg"},
-    	{"url":"https://www.wog.ch/nas/screens/pc/pc_worldofwarcraftbattleforazerotha.jpg"},
-    	{"url":"http://d1vr6n66ssr06c.cloudfront.net/wp-content/uploads/2018/04/WoW-Battle-for-Azeroth.jpg"},
-    	{"url":"https://www.instant-gaming.com/images/products/2370/screenshot/2370-5.jpg"},
-    	{"url":"https://www.instant-gaming.com/images/products/2370/screenshot/2370-4.jpg"},
-    	{"url":"https://bnetcmsus-a.akamaihd.net/cms/gallery/CQKUH7BA8SZX1517346270347.jpg"},
-    	{"url":"https://bnetcmsus-a.akamaihd.net/cms/gallery/M4V2VSHT7WFP1509668971084.jpg"},
-    	{"url":"https://bnetcmsus-a.akamaihd.net/cms/gallery/Y63IZMLIVOK21509668970773.jpg"}
-          ];
-     }
-=======
-
   images;
   videos;
 
-  constructor(private sanitizer: DomSanitizer) { }
->>>>>>> master
+constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.images = [
@@ -62,12 +39,4 @@ export class MediaComponent implements OnInit {
       {url: 'https://www.youtube.com/embed/Td0pUwrBWjc'},
     ]
   }
-  
-
-  @Input() datasource;
-    selectedImage;
-
-    setSelectedImage(image){
-       this.selectedImage= image;
-    }
 }
